d9419ccd8434a87a6e80ff1ad0e84675467e53e2
seanoughton <seanoughton@gmail.com>
2018-11-13 15:53:51 -0800

    created Transaction Model and Migration

---
+++ db/migrate/20181113235150_create_transactions.rb
@@ -0,0 +1,12 @@
+class CreateTransactions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :transactions do |t|
+      t.string :description
+      t.date :date
+      t.string :type
+      t.integer :user_id
+      t.ingteger :account_id
+      t.timestamps
+    end
+  end
+end
